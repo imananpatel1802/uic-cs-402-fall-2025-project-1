51d82b635073b547e7b4c17922bad6aa54d36acf
imananpatel1802 <47051365+imananpatel1802@users.noreply.github.com>
2025-11-12 23:16:08 -0600

    Update Firstname_Lastname_project2.cpp
    ba1 and ba2

--- project-2/Firstname_Lastname_project2.cpp
+++ project-2/Firstname_Lastname_project2.cpp
@@ -97,9 +97,24 @@ vector<unsigned int> birthday_attack_1(function<unsigned short(unsigned int)> ha
     // Note you can implement your own test hash functions so long as their 
     // signatures match the `test_hash` function signature.
     
-    // Your code here!
-    
-    return {0,1};
+    unsigned int tort = 0;
+    unsigned int hare = 0;
+
+    do {
+        tort = hash_function(tort);
+        hare = hash_function(hash_function(hare));
+    } while (tort != hare);
+
+    //cout << "tort: " << tort << " hare: " << hare << endl;
+
+    tort = 0;
+
+    while (hash_function(tort) != hash_function(hare)) {
+        tort = hash_function(tort);
+        hare = hash_function(hare);
+    }
+
+    return {tort, hare};
 }
 
 
